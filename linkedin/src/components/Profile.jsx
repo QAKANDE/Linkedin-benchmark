69f2bbf1cc36520f734f5251344411d21a879db0
QOA <58850207+QAKANDE@users.noreply.github.com>
2020-09-11 10:30:37 +0100

    update

--- linkedin/src/components/Profile.jsx
+++ linkedin/src/components/Profile.jsx
@@ -64,65 +64,72 @@ class Profile extends Component {
 
   render() {
     return (
-      <div>
-        <Jumbotron className="jumbo"></Jumbotron>
-        <div className="d-flex flex-row" style={{ height: "75px" }}>
-          <div id="imageWrapper">
-            <div id="profile-image-wrapper">
-              <img
-                id="profilePic"
-                src={this.state.details.image}
-                alt="Profile pic"
-              />
-              <div id="bottom-right">
-                <label htmlFor="file-input">
-                  <i class="fa fa-camera fa-2x " id="edit-profile-icon"></i>
-                </label>
-                <input
-                  id="file-input"
-                  type="file"
-                  profile="file"
-                  onChange={(e) => this.handleChange(e)}
-                  accept="image/*"
-                />
+      <>
+        {this.state.details.map((detail, index) => {
+          return (
+            <div key={index}>
+              <Jumbotron className="jumbo"></Jumbotron>
+              <div className="d-flex flex-row" style={{ height: "75px" }}>
+                <div id="imageWrapper">
+                  <div id="profile-image-wrapper">
+                    <img id="profilePic" src={detail.image} alt="Profile pic" />
+                    <div id="bottom-right">
+                      <label htmlFor="file-input">
+                        <i
+                          class="fa fa-camera fa-2x "
+                          id="edit-profile-icon"
+                        ></i>
+                      </label>
+                      <input
+                        id="file-input"
+                        type="file"
+                        profile="file"
+                        onChange={(e) => this.handleChange(e)}
+                        accept="image/*"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div id="editWrapper" className="ml-auto d-flex flex-row">
+                  <div className="my-3">
+                    <DropdownButton
+                      variant="outline-secondary"
+                      title="Add Profile Section"
+                      id="input-group-dropdown-1"
+                    >
+                      <Dropdown.Item href="#">Action</Dropdown.Item>
+                      <Dropdown.Item href="#">Another action</Dropdown.Item>
+                      <Dropdown.Item href="#">
+                        Something else here
+                      </Dropdown.Item>
+                      <Dropdown.Divider />
+                      <Dropdown.Item href="#">Separated link</Dropdown.Item>
+                    </DropdownButton>
+                  </div>
+
+                  <div className="m-3">
+                    <Button className="morebtn">More...</Button>
+                  </div>
+                </div>
+              </div>
+              <div className="container d-flex justify-content-between">
+                <h3 className="ml-3">
+                  {this.state.details.name} {this.state.details.surname}
+                </h3>
+                <span className="ml-3">{this.state.details.email}</span>
+              </div>
+              <div className="container d-flex justify-content-between">
+                <h5 style={{ fontWeight: "normal" }} className="ml-3">
+                  {this.state.details.area}
+                </h5>
+                <h5 style={{ fontWeight: "normal" }} className="ml-3">
+                  {this.state.details.title}
+                </h5>
               </div>
             </div>
-          </div>
-          <div id="editWrapper" className="ml-auto d-flex flex-row">
-            <div className="my-3">
-              <DropdownButton
-                variant="outline-secondary"
-                title="Add Profile Section"
-                id="input-group-dropdown-1"
-              >
-                <Dropdown.Item href="#">Action</Dropdown.Item>
-                <Dropdown.Item href="#">Another action</Dropdown.Item>
-                <Dropdown.Item href="#">Something else here</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#">Separated link</Dropdown.Item>
-              </DropdownButton>
-            </div>
-
-            <div className="m-3">
-              <Button className="morebtn">More...</Button>
-            </div>
-          </div>
-        </div>
-        <div className="container d-flex justify-content-between">
-          <h3 className="ml-3">
-            {this.state.details.name} {this.state.details.surname}
-          </h3>
-          <span className="ml-3">{this.state.details.email}</span>
-        </div>
-        <div className="container d-flex justify-content-between">
-          <h5 style={{ fontWeight: "normal" }} className="ml-3">
-            {this.state.details.area}
-          </h5>
-          <h5 style={{ fontWeight: "normal" }} className="ml-3">
-            {this.state.details.title}
-          </h5>
-        </div>
-      </div>
+          );
+        })}
+      </>
     );
   }
 }
